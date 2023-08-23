e2bf5c2521f166c85d410886d146909b9eca89d5
Khagan95 <85906243+Khagan95@users.noreply.github.com>
2023-08-23 18:11:35 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Linq;
+
+namespace FarkHesaplamaUygulamasi
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("n tane sayı girin:");
+            int[] sayilar = Console.ReadLine().Split(' ').Select(int.Parse).ToArray();
+
+            FarkHesaplayici farkHesaplayici = new FarkHesaplayici();
+            int sonuc = farkHesaplayici.FarklariHesapla(sayilar);
+
+            Console.WriteLine("Sonuç: " + sonuc);
+        }
+    }
+
+    class FarkHesaplayici
+    {
+        public int FarklariHesapla(int[] sayilar)
+        {
+            int toplamFark = 0;
+
+            foreach (int sayi in sayilar)
+            {
+                if (sayi < 67)
+                {
+                    toplamFark += 67 - sayi;
+                }
+                else
+                {
+                    toplamFark += (int)Math.Pow(sayi - 67, 2);
+                }
+            }
+
+            return toplamFark;
+        }
+    }
+}
